57b52575b78e7b8e2db48c4baaf66f334b7ccc5e
YangLonewolf <104143809+YangLonewolf@users.noreply.github.com>
2023-04-13 01:31:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+os.system("sudo apt update -y ")
+os.system("sudo apt full-upgrade -y ")
+os.system("sudo apt install kali-tweaks ")
+os.system("kali-tweaks ")
+os.system("sudo apt install -y kali-linux-default ")
+os.system("sudo apt install arduino -y ")
+os.system("sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get install kali-linux-full ")
